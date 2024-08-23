f2099b5d4c4f9897d685ec9187165f7be910e1ed
mjanez <96422458+mjanez@users.noreply.github.com>
2024-08-23 08:51:09 +0200

    Add new test image
    - Improve extension testing across environments.
    - This commit adds new custom bash scripts adapted from NHM.
    
    The purpose of these changes is to improve the reliability and stability of the CKAN Docker Spatial environment by ensuring that the required services are fully operational before the main container starts its execution.
    
    Scripts reference: [nhm-docker-ckan](https://github.com/NaturalHistoryMuseum/nhm-docker-ckan)

---
+++ common/waits/all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+bash "${BASH_SOURCE%/*}/services/postgresql.sh"
+bash "${BASH_SOURCE%/*}/services/solr.sh"
+bash "${BASH_SOURCE%/*}/services/redis.sh"
+bash "${BASH_SOURCE%/*}/services/elasticsearch.sh"
+bash "${BASH_SOURCE%/*}/services/mongodb.sh"
+
+echo "Setting DB permissions..."
+export PGPASSWORD=${PGPASSWORD:-"password"}
+ckan -c test.ini datastore set-permissions | psql -h db -U ckan
+echo "Permissions set"
+
+echo "All services up, running command"
+
+exec "$@"
---
+++ common/waits/basic.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+bash "${BASH_SOURCE%/*}/services/postgresql.sh"
+bash "${BASH_SOURCE%/*}/services/solr.sh"
+bash "${BASH_SOURCE%/*}/services/redis.sh"
+
+echo "Setting DB permissions..."
+export PGPASSWORD=${PGPASSWORD:-"password"}
+ckan -c test.ini datastore set-permissions | psql -h db -U ckan
+echo "Permissions set"
+
+echo "All services up, running command"
+
+exec "$@"
